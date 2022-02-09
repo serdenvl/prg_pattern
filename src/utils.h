67dbda7ae172ef4e01d77524b9bf1ccef62e50d3
serdenvl <serdenvl@gmail.com>
2022-02-09 17:39:26 +0700

    + def guard

--- src/utils.h
+++ src/utils.h
@@ -1,3 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <malloc.h>
@@ -303,3 +306,5 @@ int max(int a, int b)
 {
     return (a > b) ? a : b;
 }
+
+#endif
